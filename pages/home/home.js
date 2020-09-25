0088444198d50427f197bd67a43e6b6ee2e5396b
guowuhou <443076555@qq.com>
2020-09-25 17:24:57 +0800

    常见的事件,事件参数等

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles: ['衣服','裤子','鞋子']
   },
-
+  handleBtnClick() {
+    console.log('发生了点击事件')
+  },
+  handleTouchStart() {
+      console.log('handleTouchStart')
+  },
+  handleTouchMove() {
+      console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+      console.log('handleTouchEnd')
+  },
+  handleTag() {
+      console.log('handleTag')
+  },
+  handleLongPress() {
+     console.log('handleLongPress')
+  },
+  handleClickFun(event) {
+     console.log(event);
+  },
+  handleEndFun(event){
+    console.log('------',event)
+  },
+  boxOuterFun(event){
+    console.log(event)
+  },
+  boxInnerFun(event){
+    console.log(event)
+  },
+  handleItemClick(event){
+    console.log(event);
+    console.log(event.currentTarget.dataset.item);
+    console.log(event.currentTarget.dataset.index);
+  },
+  handleCaptureview1() {
+    console.log('handleCaptureview1')
+  },
+  handleCaptureview2() {
+    console.log('handleCaptureview2')
+  },
+  handleCaptureview3() {
+    console.log('handleCaptureview3')
+  },
+  handleView1() {
+    console.log('handleView1')
+  },
+  handleView2() {
+    console.log('handleView2')
+  },
+  handleView3() {
+    console.log('handleView3')
+  },
   /**
    * 生命周期函数--监听页面加载
    */
