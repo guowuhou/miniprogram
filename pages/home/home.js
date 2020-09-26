137aa92ed48252c43900a9d2890e70f62f7e92e4
guowuhou <443076555@qq.com>
2020-09-26 15:53:28 +0800

    小程序网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
 
   /**
@@ -12,7 +13,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //1.发送网络请求get默认是get 原生的方法
+      // wx.request({
+      //   // url: 'http://123.207.32.32:8000/recommend',
+      //   url: 'http://123.207.32.32:8000/home/data',
+      //   data: {type: 'sell', page: 1},
+      //   success: (res) => {
+      //       console.log(res);
+      //   }
+      // });
+      //  //发送网络请求post默认是get
+      //  wx.request({
+      //   url: 'http://httpbin.org/post',
+      //   data: {name: 'sell', age: 18},
+      //   method: 'post',
+      //   success: (res) => {
+      //       console.log(res);
+      //   },
+      //   fail: (err) => {
+      //     console.log(err)
+      //   }
+      // })
+    //2.使用封装的request发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/recommend'
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
   },
 
   /**
